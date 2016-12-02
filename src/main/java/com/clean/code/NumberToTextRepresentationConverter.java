40031b6814633feb1856ca31c711e8d1b47acd5d
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-02 19:20:14 +0530

    REFACTOR - Invoke TextGenerator to generate textual representation

--- src/main/java/com/clean/code/NumberToTextRepresentationConverter.java
+++ src/main/java/com/clean/code/NumberToTextRepresentationConverter.java
@@ -1,11 +1,16 @@
 package com.clean.code;
 
-public class NumberToTextRepresentationConverter {
+import com.clean.code.text.TextGenerator;
 
-	public String textualRepresentation(int i) {
-		if(i == 0){
-			return "Zero";
-		}
-		return "One";
+public class NumberToTextRepresentationConverter {
+	
+	private TextGenerator textGenerator;
+	
+	public NumberToTextRepresentationConverter(){
+		this.textGenerator = new TextGenerator();
+	}
+
+	public String textualRepresentation(int number) {
+		return textGenerator.generateText(number);
 	}
 }
