80d5b4bec77201881ee4734c076c41cffe42d6bb
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-02 19:18:21 +0530

    REFACTOR - TextGenerator created for generating text.

---
+++ src/main/java/com/clean/code/text/TextGenerator.java
@@ -0,0 +1,14 @@
+package com.clean.code.text;
+
+public class TextGenerator {
+	
+	public String generateText(int number){
+		
+		if(number == 0){
+			return "Zero";
+		}
+		return "One";
+		
+	}
+
+}
