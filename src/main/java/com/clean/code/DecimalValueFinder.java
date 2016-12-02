683548734548faa32205a5f1af741988324c3567
harsh2015 <harishpanduranga.puthran@hotmail.com>
2016-12-03 01:11:41 +0530

    GREEN - Returns Twenty One for Input Number 21.

--- src/main/java/com/clean/code/DecimalValueFinder.java
+++ src/main/java/com/clean/code/DecimalValueFinder.java
@@ -5,12 +5,50 @@ import java.util.List;
 
 public class DecimalValueFinder {
 	
-	public List<Integer> retrieveDecimalValues(int number){
-		List<Integer> decimalValues = new ArrayList<>();
-		int valueAtTensPlace = number / 10;
-		int valueAtUnitsPlace = number % 10;		
-		decimalValues.add(valueAtUnitsPlace);
-		decimalValues.add(valueAtTensPlace);
-		return decimalValues;
+	private static final String EMPTY_STRING_LITERAL = "";
+	private static final String ZERO_TEXT = "Zero";
+	private static final String NUMBER_0 = "0";
+	private String[] TENS_COMMONTEXT = {"Ten", "Twenty"};
+	private String[] UNITS_TEXT = {"One", "Two"};
+	
+	public List<String> retrieveDecimalValues(int number){
+		
+		List<String> decimalValuesInText = new ArrayList<>(10);		
+		String numbericString = String.valueOf(number);		
+		int numberOfDigits = numbericString.length();
+		
+		switch(numberOfDigits){
+			case 1: {
+						decimalValuesInText.add(findUnitsPlace(numbericString, Boolean.FALSE)); 
+						break;
+					 }
+			case 2:  {
+						decimalValuesInText.add(findTensPlace(numbericString.substring(0,1)));
+						decimalValuesInText.add(findUnitsPlace(numbericString.substring(1, 2), Boolean.TRUE));
+						break;
+					 }			
+			}
+			return decimalValuesInText;
+		}	
+
+	private String findTensPlace(String inputNumberAtTensPosition) {
+		if(!inputNumberAtTensPosition.equals(0)){
+			return TENS_COMMONTEXT[Integer.valueOf(inputNumberAtTensPosition) - 1];
+		}
+		return EMPTY_STRING_LITERAL;
+		
 	}
+
+	private String findUnitsPlace(String inputNumberAtUnitsPosition, Boolean zeroTextExclusionFlag) {
+		if(inputNumberAtUnitsPosition.equals(NUMBER_0)){
+			if(!zeroTextExclusionFlag){
+				return ZERO_TEXT;
+			}
+			return EMPTY_STRING_LITERAL;
+		}
+		return UNITS_TEXT[Integer.valueOf(inputNumberAtUnitsPosition) - 1];
+			
+		
+	}
+
 }
--- src/main/java/com/clean/code/text/TextGenerator.java
+++ src/main/java/com/clean/code/text/TextGenerator.java
@@ -7,26 +7,22 @@ import com.clean.code.DecimalValueFinder;
 public class TextGenerator {
 	
 	private DecimalValueFinder decimalValueFinder;
-	private String[] TENS_TEXT = {"", "Ten", "Twenty"};
-	private String[] UNITS_TEXT = {"Zero", "One", "Two"};
+
 	
 	public TextGenerator(){
 		this.decimalValueFinder = new DecimalValueFinder();
 	}
 	
 	public String generateText(int number){	
-		List<Integer> decimalValues = this.decimalValueFinder.retrieveDecimalValues(number);
+		List<String> decimalValues = this.decimalValueFinder.retrieveDecimalValues(number);
 		StringBuilder builder = new StringBuilder();
-		String unitPlaceText = "";
-		if(decimalValues.get(1) != 0){
-			builder.append(TENS_TEXT[decimalValues.get(1)]);
-			unitPlaceText = decimalValues.get(0) != 0 ? 
-							UNITS_TEXT[decimalValues.get(0)] : "";
-		}else{
-			unitPlaceText = UNITS_TEXT[decimalValues.get(0)];
-		}
-		
-		builder.append(unitPlaceText);
-		return builder.toString();		
+		decimalValues.stream().forEach((textValue) -> {
+			{
+				builder.append(textValue);
+				builder.append(" ");
+			}
+		});
+		String generateText = builder.toString();
+		return generateText.trim();		
 	}
 }
