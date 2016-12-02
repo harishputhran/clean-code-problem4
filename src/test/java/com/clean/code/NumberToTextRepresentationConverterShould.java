7197f9869d3605089b66f541e6be1e7d4602ed81
kg00 <kg00@INLTD12229.global.tesco.org>
2016-12-02 17:05:09 +0530

    RED - Scenario 1 - Return Zero for number 0

---
+++ src/test/java/com/clean/code/NumberToTextRepresentationConverterShould.java
@@ -0,0 +1,16 @@
+package com.clean.code;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class NumberToTextRepresentationConverterShould {
+	
+	@Test
+	public void return_zero_text_when_input_number_is_0(){
+		NumberToTextRepresentationConverter numberToTextConverter = new NumberToTextRepresentationConverter();
+		assertEquals("Zero", numberToTextConverter.textualRepresentation(0));
+	}
+
+}
